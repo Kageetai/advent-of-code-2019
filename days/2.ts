871ba46be59045c78e1df045579eb193075c75ea
Michael Seifarth <Kageetai@users.noreply.github.com>
2019-12-17 17:36:52 +0100

    refactor

--- days/2.ts
+++ days/2.ts
@@ -2,20 +2,31 @@ import { chunk } from 'lodash';
 
 import input from '../inputs/2.json';
 
-const executeIntcode = (code: number[]) => {
-  switch (code[0]) {
-    case 1:
-      input[code[3]] = input[code[1]] + input[code[2]];
-      break;
-    case 2:
-      input[code[3]] = input[code[1]] * input[code[2]];
-      break;
-    case 99:
-      // console.log('stop');
-      break;
-  }
+const executeProgram = (program: number[], noun: number, verb: number) => {
+  program[1] = noun;
+  program[2] = verb;
+  const chunks = chunk(program, 4);
+
+  (function() {
+    for (let i = 0; i < chunks.length; i++) {
+      const code = chunks[i];
+
+      switch (code[0]) {
+        case 1:
+          program[code[3]] = program[code[1]] + program[code[2]];
+          break;
+        case 2:
+          program[code[3]] = program[code[1]] * program[code[2]];
+          break;
+        case 99:
+          return;
+      }
+    }
+  })();
+
+  return program;
 };
 
-chunk(input, 4).forEach(executeIntcode);
+const result = executeProgram([...input], 12, 2);
 
-export default input;
+export default result[0];
